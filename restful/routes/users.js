35204fbccb7345ec19a4cca0014601567c87c557
rafael <rafaelalves78@gmail.com>
2020-09-09 22:32:38 -0300

    Carregando rotas com Consign

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -1,26 +1,27 @@
-let express = require('express');
-let routes = express.Router();
 
-routes.get('/',(req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    res.json({
 
-        users: [{
-            name:'Rafael',
-        }]
+module.exports = app=>{
+
+    app.get('/users',(req,res)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json({
+    
+            users: [{
+                name:'Rafael',
+            }]
+        });
     });
-});
-
-routes.get('/admin', (req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    res.json({
-
-        users: [{
-           
-        }]
+    
+    app.get('/users/admin', (req,res)=>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type','application/json');
+        res.json({
+    
+            users: [{
+               
+            }]
+        });
     });
-});
-
-module.exports = routes;
+    
+};
