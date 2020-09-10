77b7cbbeb25925269310c29039b79485c393452b
rafael <rafaelalves78@gmail.com>
2020-09-09 22:09:43 -0300

    Separando rotas do arquivo principal

--- restful/index.js
+++ restful/index.js
@@ -1,24 +1,11 @@
 const express = require('express');
+let routesIndex = require('./routes/index');
+let routesUsers = require('./routes/users')
 
 let app = express();
 
-app.get('/', (req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-type','text/html');
-    res.end('<h1>Ola</h1>');
-
-});
-app.get('/users',(req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type','application/json');
-    res.json({
-
-        users: [{
-            name:'Rafael',
-        }]
-    });
-})
-
+app.use(routesIndex);
+app.use('/users',routesUsers);
 
 app.listen(3000,'127.0.0.1',()=>{
     console.log('servidor rodando!')
