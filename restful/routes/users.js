ed4233671e1273660e373b07265f36bf02383667
rafael <rafaelalves78@gmail.com>
2020-09-10 23:38:29 -0300

    Validando dados recebidos via Post

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -31,7 +31,7 @@ module.exports = app=>{
     
     route.post((req,res)=>{
        
-        
+        if (!app.utils.validator.user(app, req, res)) return false;
         db.insert(req.body, (err,user)=>{
 
             if(err){
@@ -57,6 +57,10 @@ module.exports = app=>{
     });
 
     routeId.put((req, res) =>{
+
+        if(!app.utils.validator.user(app, req, res)) return false;
+
+
         db.update({_id: req.params.id},req.body, err=>{
             if(err){
                 app.utils.error.send(err, req, res);        
