59b1abddbb2eb96e54f3f47eda3362633f1669bc
rafael <rafaelalves78@gmail.com>
2020-09-10 21:21:58 -0300

    Persistencia de dados com o NeDB

--- restful/routes/users.js
+++ restful/routes/users.js
@@ -1,4 +1,8 @@
-
+let NeDB = require('nedb');
+let db = new NeDB({
+     filename: "users.db",
+     autoload: true
+});
 
 module.exports = app=>{
 
@@ -13,15 +17,21 @@ module.exports = app=>{
         });
     });
     
-    app.get('/users/admin', (req,res)=>{
-        res.statusCode = 200;
-        res.setHeader('Content-Type','application/json');
-        res.json({
-    
-            users: [{
-               
-            }]
+    app.post('/users', (req,res)=>{
+       
+        
+        db.insert(req.body, (err,user)=>{
+
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+            }else{
+                res.status(200).json(user);
+            }
         });
+
     });
     
 };
