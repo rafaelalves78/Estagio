89fc59b2a3296bacefe02b457d45466871112700
rafael <rafaelalves78@gmail.com>
2020-09-15 19:04:52 -0300

    Consumindo Rotas, POST, PUT, DELETE com Restify

--- gerenciamento-ususario-RESTfull/client-server/routes/users.js
+++ gerenciamento-ususario-RESTfull/client-server/routes/users.js
@@ -14,9 +14,52 @@ router.get('/', function(req, res, next) {
   client.get('/users', function(err, request, resposnse, obj) {
     assert.ifError(err);
   
-    res.end(JSON.stringify(obj, null, 2));
+    res.json(obj);
   });
 
 });
+
+router.get('/:id', function(req, res, next) {
+  
+  client.get(`/users/${req.params.id}`, function(err, request, resposnse, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+  
+  client.put(`/users/${req.params.id}`, req.body,function(err, request, resposnse, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+
+});
+
+router.delete('/:id', function(req, res, next) {
+  
+  client.del(`/users/${req.params.id}`, function(err, request, resposnse, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+});
+
+router.post('/', function(req, res, next) {
+  
+  client.post(`/users`,req.body, function(err, request, resposnse, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+
+  });
+});
+
+
 
 module.exports = router;
