35f02d1f812251aa752f332c6d89c5fe081846a7
rafael <rafaelalves78@gmail.com>
2020-09-19 10:03:45 -0300

    Enviando arquivos usando Ajax e Promise.all()

--- DropBox Clone/app/public/src/controller/DropBoxController.js
+++ DropBox Clone/app/public/src/controller/DropBoxController.js
@@ -4,6 +4,8 @@ class DropBoxController{
 
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
+        this.snakModalEl = document.querySelector('#react-snackbar-root');
+
         this.initEvent();
 
     }
@@ -16,8 +18,54 @@ class DropBoxController{
             this.inputFilesEl.click();
 
 
-        } )
+        });
+        this.inputFilesEl.addEventListener('change', event =>{
+
+           this.uploadTask(event.target.files);
+
+           this.snakModalEl.style.display ="block";
+
+        });   
+    }
+
+    uploadTask(files){
     
+        let promises = [];
+
+        [...files].forEach(file=>{
+           
+            promises.push(new Promise((resolve, reject)=>{
+
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+                ajax.onload = event =>{
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) {
+                       reject(e); 
+                    }
+                };
+                ajax.onerror = event =>{
+                    reject(event);
+                };
+
+                let formData = new FormData();
+
+                formData.append('input-file', file);
+               
+                ajax.send(formData);
+
+
+
+            }));
+        })
+
+
+
+        return Promise.all(promises);
+
+
     }
 
 }
