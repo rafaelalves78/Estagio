2a9b5de951339f1e8c6786c30afbffd5e54149b2
rafael <rafaelalves78@gmail.com>
2020-09-21 20:58:45 -0300

    Selecionando arquivos com CTRL OU SHIFT

--- DropBox Clone/app/public/src/controller/DropBoxController.js
+++ DropBox Clone/app/public/src/controller/DropBoxController.js
@@ -12,7 +12,7 @@ class DropBoxController{
        
         this.connectFirebase();
         this.initEvent();
-        this.readFile();
+        this.readFiles();
 
     }
     connectFirebase(){
@@ -354,15 +354,16 @@ class DropBoxController{
 
         li.innerHTML = `
           
-                ${this.getFileIconView(file)}
-                <div class="name text-center">${file.name}</div>
-   
-            `
+            ${this.getFileIconView(file)}
+            <div class="name text-center">${file.name}</div>
+
+        `
+        this.initEventsLi(li);
 
         return li;
     }
 
-    readFile(){
+    readFiles(){
         this.getFirebaseRef().on('value', snapshot =>{
 
             this.listFilesEl.innerHTML = '';
@@ -376,6 +377,51 @@ class DropBoxController{
             })
 
 
+        })
+    }
+
+    initEventsLi(li){
+    
+        li.addEventListener('click', e =>{
+
+
+            if(e.shiftKey){
+                let firstLi = this.listFilesEl.querySelector('.selected');
+
+                if(firstLi){
+                    let indexStart;
+                    let indexEnd;
+                    let lis = li.parentElement.childNodes;
+
+                  lis.forEach((el, index) =>{
+
+                    if(firstLi === el) indexStart = index;
+                    if(li === el) indexEnd = index;
+
+                  });
+
+                  let index = [indexStart , indexEnd].sort();
+                 
+                  lis.forEach((el, i) =>{
+
+                    if(index >= index[0] && i<= index[1]){
+                        el.classList.add('selected')
+                    }
+
+                  });
+
+
+                  return true;
+                }
+
+            }
+            if (!e.ctrlKey) {
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el =>{
+                    el.classList.remove('selected');
+                })
+            }
+
+            li.classList.toggle('selected')
         })
     }
 
