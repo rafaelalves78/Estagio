184320465f65bc8d18e6dbf6a649ddabd58610fe
rafael <rafaelalves78@gmail.com>
2020-09-29 20:35:00 -0300

    Prototype

--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -2,7 +2,7 @@ class WhatsAppController{
 
     constructor(){
 
-        console.log("WhatsAppController OK")
+        this.elementsPrototype();
         this.loadElements();
     }
 
@@ -15,6 +15,58 @@ class WhatsAppController{
 
             this.el[Format.getCamelCase(element.id)] = element;
         });
+
+    }
+
+    elementsPrototype(){
+
+        Element.prototype.hide = function(){
+            this.style.display = 'none';
+            return this;
+        }
+
+        Element.prototype.show = function(){
+            this.style.display = 'block';
+            return this;
+        }
+
+        Element.prototype.toggle = function(){
+            this.style.display = (this.style.display === 'none') ? 'block' : 'none';
+            return this;
+        }
+
+        Element.prototype.on = function(events, fn){
+            events.split(' ').forEach(event=>{
+                this.addEventListener(event, fn)
+            });
+            return this;            
+        }
+       
+        Element.prototype.css = function(styles){
+            for (let name in styles){
+                this.style[name] = styles[name]
+            }
+            return this;
+        }
+       
+        Element.prototype.addClass = function(name){
+            this.classList.add(name);
+            return this;
+        }
+       
+        Element.prototype.removeClass = function(name){
+            this.classList.remove(name);
+            return this;
+        }
+
+        Element.prototype.toggleClass = function(name){
+            this.classList.toggle(name);
+            return this;
+        }
+
+        Element.prototype.hasClass = function(name){
+            this.classList.contains(name);
+        }
 
     }
 
