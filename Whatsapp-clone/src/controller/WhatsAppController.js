bbe6769ba062999589a3b29518160069e5fd2469
rafael <rafaelalves78@gmail.com>
2020-10-01 17:59:50 -0300

    Eventos para abrir/fechar os paineis (Editar Perfil) e (Adicionar Contato)

--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController{
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     loadElements(){
@@ -68,6 +69,42 @@ class WhatsAppController{
             this.classList.contains(name);
         }
 
+    }
+
+    initEvents(){
+        this.el.myPhoto.on('click', e=>{
+ 
+            this.closeAllLeftPanel();
+            this.el.panelEditProfile.show();
+            setTimeout(()=>{
+                this.el.panelEditProfile.addClass('open');
+            }, 300);
+            
+        });
+        
+        this.el.btnNewContact.on('click', e=>{
+
+            this.closeAllLeftPanel();
+            this.el.panelAddContact.show();
+            setTimeout(()=>{
+                this.el.panelAddContact.addClass('open');
+            }, 300);
+            
+        });
+
+        this.el.btnClosePanelEditProfile.on('click', e=>{
+
+            this.el.panelEditProfile.removeClass('open');
+        });
+
+        this.el.btnClosePanelAddContact.on('click', e =>{
+            this.el.panelAddContact.removeClass('open');
+        })
+    }
+    closeAllLeftPanel(){
+
+        this.el.panelAddContact.hide();
+        this.el.panelEditProfile.hide();
     }
 
 
