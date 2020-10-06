0c4c53fe182f02e8d5790f3e19cb8de6dad8b231
rafael <rafaelalves78@gmail.com>
2020-10-05 21:10:41 -0300

    Tirar fotografia da câmera com canvas

--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -184,6 +184,26 @@ export  class WhatsAppController{
 
         this.el.btnTakePicture.on('click', e=>{
 
+           let dataUrl =  this._camera.takePicture();
+
+           this.el.pictureCamera.src = dataUrl;
+           this.el.pictureCamera.show();
+           this.el.videoCamera.hide();
+           this.el.btnReshootPanelCamera.show();
+           this.el.containerTakePicture.hide();
+           this.el.containerSendPicture.show();
+        });
+
+        this.el.btnReshootPanelCamera.on('click', e=>{
+           this.el.pictureCamera.hide();
+           this.el.videoCamera.show();
+           this.el.btnReshootPanelCamera.hide();
+           this.el.containerTakePicture.show();
+           this.el.containerSendPicture.hide();
+        });
+
+        this.el.btnSendPicture.on('click', e=>{
+            
         })
 
         this.el.btnAttachDocument.on('click', e=>{
