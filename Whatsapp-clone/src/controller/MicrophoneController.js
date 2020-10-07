aed3509d7021f8ecc9ef83cf7a66b12c36a8c301
rafael <rafaelalves78@gmail.com>
2020-10-07 18:49:24 -0300

    Gravando audio / criando evento para visualizar o tempo de gravação

--- Whatsapp-clone/src/controller/MicrophoneController.js
+++ Whatsapp-clone/src/controller/MicrophoneController.js
@@ -8,17 +8,20 @@ export class MicrophoneController extends ClassEvent{
 
         super();
 
+        this._available = false
+
         navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+
+            this._available = true
+                     
 
             this._stream = stream;
 
-            let audio = new Audio();
-
-           // audio.src = URL.createObjectURL(stream)
-           
-           audio.srcObject = stream
-           audio.play();
-           this.trigger('play', audio);     
+          
+            this.trigger('ready', {
+                sream: this._stream,
+                audio: this._audio
+            });
 
         }).catch(err => {
 
@@ -27,10 +30,89 @@ export class MicrophoneController extends ClassEvent{
         });
 
     }
+
+    isAvailable(){
+        return this._available;
+    }
     stop(){
         this._stream.getTracks().forEach(track =>{
             track.stop();
         })
+    }
+
+    startRecorder(options = {}){
+
+        if (this._available) {
+
+            this.startTimer();
+
+            this._mediaRecorder = new MediaRecorder(this._stream, Object.assign(options, {
+                mimeType: 'audio/webm'
+            }));
+
+            this._recordedChunks = [];
+
+            this._mediaRecorder.addEventListener('dataavailable', e => {
+
+                if (e.data.size > 0) this._recordedChunks.push(e.data);
+
+            });
+
+            this._mediaRecorder.addEventListener('stop', () => {
+
+                let blob = new Blob(this._recordedChunks, {
+                    type: 'audio/webm'
+                });
+
+                let cx = new AudioContext();
+
+                var fileReader = new FileReader();
+                
+                fileReader.onload = e => {
+
+                    cx.decodeAudioData(fileReader.result).then(decode => {
+
+                        let file = new File([blob], 'rec' + new Date().getTime() + '.webm', {
+                            type: 'audio/webm',
+                            lastModified: Date.now()
+                        });
+
+                        this.trigger('recorded', file, decode);
+
+                    });
+
+                };
+
+                fileReader.readAsArrayBuffer(blob);                
+
+            });
+
+            this._mediaRecorder.start();
+
+        }
+
+    }
+
+    stopRecorder(){
+
+        if(this.isAvailable()){
+
+            this._mediaRecorder.stop();
+            this.stop()
+            this.stopTimer()
+
+        }
+    }
+
+    startTimer(){
+        let start = Date.now();
+        this._recordMicrophoneInterval = setInterval(() =>{
+            this.trigger('recordtimer', (Date.now() - start));
+        }, 100);
+    }
+
+    stopTimer(){
+        clearInterval(this._recordMicrophoneInterval)
     }
 
 
--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -299,25 +299,32 @@ export  class WhatsAppController{
         this.el.btnSendMicrophone.on('click', e=>{
             this.el.recordMicrophone.show();
             this.el.btnSendMicrophone.hide();
-            this.startRecordMicrophoneTime();
+         
 
             this._microphoneController = new MicrophoneController();
 
-            this._microphoneController.on('play', musica=>{
+            this._microphoneController.on('ready', musica=>{
 
-                console.log('recebi' , musica)
+                this._microphoneController.startRecorder();
+            });
+
+            this._microphoneController.on('recordtimer', timer =>{
+
+                this.el.recordMicrophoneTimer.innerHTML = Format.toTime( timer)
             })
+
+
         });
 
         this.el.btnCancelMicrophone.on('click', e=>{
 
-           this._microphoneController.stop();
+           this._microphoneController.stopRecorder();
            this.closeRecordMicrophone();
         });
 
         this.el.btnFinishMicrophone.on('click', e=>{
 
-           this._microphoneController.stop();
+           this._microphoneController.stopRecorder();
            this.closeRecordMicrophone();
         });
 
@@ -393,19 +400,13 @@ export  class WhatsAppController{
         })
 
     }
-    startRecordMicrophoneTime(){
-
-        let start = Date.now();
-        this._recordMicrophoneInterval = setInterval(() =>{
-            this.el.recordMicrophoneTimer.innerHTML = Format.toTime( Date.now() - start)
-        }, 100);
-    }
+ 
 
     closeRecordMicrophone(){
         
         this.el.recordMicrophone.hide();
         this.el.btnSendMicrophone.show();
-        clearInterval(this._recordMicrophoneInterval)
+      
     }
 
     closeAllMainPanel(){
