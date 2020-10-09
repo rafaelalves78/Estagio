d05b03fac4d809b62ebae5cfb0e5461a722db45a
rafael <rafaelalves78@gmail.com>
2020-10-09 18:35:49 -0300

    Enviando projeto

---
+++ Restaurante/saboroso/inc/db.js
@@ -0,0 +1,13 @@
+
+const mysql = require('mysql2');
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'restaurante',
+    password: '123456789'
+  });
+
+  module.exports = connection;
