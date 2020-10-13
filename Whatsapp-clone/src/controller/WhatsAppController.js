7deae71bdd7e2b721ba5ea0dc3b104741238e713
rafael <rafaelalves78@gmail.com>
2020-10-13 14:12:55 -0300

    Desceno o Scroll / Filtrando lista de contatos

--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -177,8 +177,14 @@ export  class WhatsAppController{
             display: 'flex'
         })
 
+        this.el.panelMessagesContainer.innerHTML = '';
+        
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs => {
-            this.el.panelMessagesContainer.innerHTML = '';
+          
+
+            let scrollTop = this.el.panelMessagesContainer.scrollTop;
+            let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
+            let autoScroll = (scrollTop >= scrollTopMax)
 
             docs.forEach(doc => {
                 let data = doc.data();
@@ -187,6 +193,7 @@ export  class WhatsAppController{
 
                 if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)){
 
+            
                     let message = new Message();
 
                     message.fromJSON(data);
@@ -196,9 +203,18 @@ export  class WhatsAppController{
                     let view = message.getViewElement(me)
 
                     this.el.panelMessagesContainer.appendChild(view)
+
+                   
                 }
                 
             })
+            if(autoScroll){
+
+                this.el.panelMessagesContainer.scrollTop = (this.el.panelMessagesContainer.scrollHeight - 
+                this.el.panelMessagesContainer.offsetHeight)
+            }else{
+                this.el.panelMessagesContainer.scrollTop = scrollTop;
+            }
         })
 
     }
