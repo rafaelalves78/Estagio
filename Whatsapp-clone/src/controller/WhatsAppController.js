f4e9fa8b0b7b2118708f7f57c3f53b6824edb110
rafael <rafaelalves78@gmail.com>
2020-10-13 12:46:01 -0300

    Enviando uma mensagem de texto

--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -5,6 +5,7 @@ import {DocumentPreviewController} from './DocumentPreviewController';
 import { Firebase } from '../util/Firebase';
 import { User } from './../model/User';
 import { Chat } from '../model/Chat';
+import { Message } from '../model/Message';
 
 
 //import { response } from 'express';
@@ -140,20 +141,7 @@ export  class WhatsAppController{
 
                  div.on('click', e =>{
 
-                    this.el.activeName.innerHTML = contact.name;
-                    this.el.activeStatus.innerHTML = contact.status;
-
-                    if(contact.photo){
-                       let img = this.el.activePhoto;
-                       img.src = contact.photo;
-                       img.show();
-                    }
-
-                    this.el.home.hide();
-                    this.el.main.css({
-                        display: 'flex'
-                    })
-
+                   this.setActiveChat(contact)
 
                  })
 
@@ -165,6 +153,26 @@ export  class WhatsAppController{
 
         this._user.getContacts();
     
+    }
+
+    setActiveChat(contact){
+
+        this._contactActive = contact;
+
+        this.el.activeName.innerHTML = contact.name;
+        this.el.activeStatus.innerHTML = contact.status;
+
+        if(contact.photo){
+           let img = this.el.activePhoto;
+           img.src = contact.photo;
+           img.show();
+        }
+
+        this.el.home.hide();
+        this.el.main.css({
+            display: 'flex'
+        })
+
     }
 
     loadElements(){
@@ -543,6 +551,11 @@ export  class WhatsAppController{
         })
 
         this.el.btnSend.on('click', e=>{
+
+            Message.send(this._contactActive.chatId,this._user.email,'text', this.el.inputText.innerHTML);
+
+            this.el.inputText.innerHTML = '';
+            this.EL.panelEmojis.removeClass('open')
 
         });
 
