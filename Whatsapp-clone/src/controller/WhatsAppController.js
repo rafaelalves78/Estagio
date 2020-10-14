3f34cab1653e39dcd2b3177209834cc7edaf76bd
rafael <rafaelalves78@gmail.com>
2020-10-14 12:11:57 -0300

    Enviando contato

--- Whatsapp-clone/src/controller/WhatsAppController.js
+++ Whatsapp-clone/src/controller/WhatsAppController.js
@@ -7,6 +7,7 @@ import { User } from './../model/User';
 import { Chat } from '../model/Chat';
 import { Message } from '../model/Message';
 import { Base64 } from "../util/Base64";
+import {ContactsController} from "../controller/ContactsController"
 
 //import { response } from 'express';
 //import { from } from 'webpack-sources/lib/CompatSource';
@@ -197,6 +198,8 @@ export  class WhatsAppController{
 
                 let me = (data.from === this._user.email);
 
+                let view = message.getViewElement(me)
+
                 if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)){
 
                     if(!me){
@@ -208,15 +211,17 @@ export  class WhatsAppController{
                         })
                     }
 
-                    let view = message.getViewElement(me)
+             
 
                     this.el.panelMessagesContainer.appendChild(view)
 
                    
                 }else {
-                    let view = message.getViewElement(me)
+                
 
-                    this.el.panelMessagesContainer.querySelector('#_' + data.id).innerHTML = view.innerHTML
+                    let parent = this.el.panelMessagesContainer.querySelector('#_' + data.id).parentNode;
+                    parent.replaceChild(view,this.el.panelMessagesContainer.querySelector('#_' + data.id) )
+
                 }
                 
                 if(this.el.panelMessagesContainer.querySelector('#_' + data.id) && me){
@@ -224,6 +229,34 @@ export  class WhatsAppController{
 
                     msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
                 }
+
+                if(message.type === 'contact'){
+
+                    view.querySelector('.btn-message-send').on('click', e=>{
+                       
+                        Chat.createIfNotExists(this._user.email, message.content.email).then(chat => {
+                       
+
+                            let contact = new User(message.content.email);
+
+                            contact.on('dataChange', data => {
+                                
+                                contact.chatId = chat.id;
+
+                                this._user.addContact(contact)
+                            
+                                this._user.chatId = chat.id;
+        
+                                contact.addContact(this._user);
+                               
+                                this.setActiveChat(contact)
+                            })
+
+                        });
+                    })
+                }
+
+                
                 
             })
             if(autoScroll){
@@ -618,12 +651,25 @@ export  class WhatsAppController{
         })
 
         this.el.btnAttachContact.on('click', e=>{
-            this.el.modalContacts.show()
+       
+
+            this._contactsController = new ContactsController(this.el.modalContacts, this._user)
+
+            this._contactsController.on('select', contact =>{
+                Message.sendContact(
+                    this._contactActive.chatId,
+                    this._user,
+                    contact
+                )
+            })
+
+            this._contactsController.open();
 
         });
 
         this.el.btnCloseModalContacts.on('click', e=>{
-            this.el.modalContacts.hide()
+            this._contactsController.close();
+         
         });
 
         this.el.btnSendMicrophone.on('click', e=>{
--- Whatsapp-clone/src/model/Message.js
+++ Whatsapp-clone/src/model/Message.js
@@ -53,6 +53,7 @@ export class Message extends Model{
        
         let div = document.createElement('div');
 
+        div.id=`_${this.id}`
         div.className = 'message'
        
         switch(this.type){
@@ -81,7 +82,7 @@ export class Message extends Model{
                             </div>
                         </div>
                         <div class="_1lC8v">
-                            <div dir="ltr" class="_3gkvk selectable-text invisible-space copyable-text">Nome do Contato Anexado</div>
+                            <div dir="ltr" class="_3gkvk selectable-text invisible-space copyable-text">${this.content.name}</div>
                         </div>
                         <div class="_3a5-b">
                             <div class="_1DZAH" role="button">
@@ -96,9 +97,15 @@ export class Message extends Model{
                 </div>
 
             </div>
-       
-                                        
             `;
+
+            if(this.content.photo){
+                let img = div.querySelector('.photo-contact-sended');
+                img.src = this.content.photo;
+                img.show();
+            }
+
+         
               
             break;
 
@@ -300,7 +307,7 @@ export class Message extends Model{
             default:
                 div.innerHTML = `
               
-                <div class="font-style _3DFk6  tail"  id="_${this.id}" >
+                <div class="font-style _3DFk6  tail"   >
                     <span class="tail-container"></span>
                     <span class="tail-container highlight"></span>
                     <div class="Tkt2p">
@@ -360,6 +367,12 @@ export class Message extends Model{
             });
 
         });
+
+    }
+
+    static sendContact(chatId, from, contact){
+
+        return Message.send(chatId, from, 'contact', contact);
 
     }
 
