b3889081c87df0196fc0d299f1cf16cdc434d27d
rafael <rafaelalves78@gmail.com>
2020-10-27 20:37:39 -0300

    criando pasta

---
+++ c#/Properties/Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+
+            Console.WriteLine("Nome: " + p.Nome);
+            Console.WriteLine("Preço: " + p.Preco);
+            Console.WriteLine("Quantidade: " + p.Quantidade);
+        }
+    }
+}
